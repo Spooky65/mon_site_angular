244b590e87fefcf396b52003aa496a5e65bfc1f6
Nicolas <nicolasdiot65@gmail.com>
2024-04-12 00:12:52 +0200

    add theme

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
@@ -9,7 +9,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,MatCardModule, MatButtonModule,MatGridListModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,MatCardModule, MatButtonModule,MatGridListModule, RouterLink],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
@@ -18,25 +18,25 @@ export class IndexComponent {
   cards = [
     {
       title: 'CV',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      description: 'Retrouver ici un aperçu de mon CV au format téléchargeable et imprimable.',
       href: 'cv',
       img: 'assets/img/cv_miniature.jpg'
     },
     {
       title: 'Pixel Idle',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      description: 'Idle game de click sur le thème des pixels.',
       href: 'pixel',
       img: 'assets/img/pixel_miniature.jpg'
     },
     {
       title: 'Movierama',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      description: 'Recherche des informations sur un film avec une API externe (OMDb).',
       href: 'movierama',
       img: 'assets/img/movierama_api_miniature.png'
     },
     {
       title: 'Timer',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      description: 'Permet de concentrer son temps sur le web, grace un onglet éphémère et limité dans le temps.',
       href: 'timer',
       img: 'assets/img/timer_miniature.webp'
     },
